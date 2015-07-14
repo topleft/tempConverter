40cd683d3f471a040afa5a23a3522c60ee3a66e7
topleft <pete.james.jeffryes@gmail.com>
2015-07-13 21:55:04 -0600

    cleaned up code, removed console.log's

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ $(document).ready( function() {
   // promt user for conversion choice (to C or to F?) and check for validity
   var userChoice = prompt("Convert to F or C? (f/c): ");
   userChoice = userChoice.toLowerCase()
-  console.log(typeof(userChoice));
+  // console.log("Type of user input: " + typeof(userChoice));
   if (userChoice != "c" && userChoice != "f") {
     userChoice = prompt("Try again, C or F please.\nConvert to F or C? (f/c): ");
   };
@@ -27,14 +27,11 @@ $(document).ready( function() {
   // then prompt for temp and check to be sure it is valid
   var userTemp = prompt("Please enter a temperature: ");
   userTemp = parseInt(userTemp);
-  console.log(typeof(userTemp));
+  // console.log(typeof(userTemp));
   if ( (typeof userTemp) != 'number') {
     userTemp = prompt("Try again, numbers only plaese.\nEnter a temperature: ");
   };
   userTemp = parseInt(userTemp);
-  console.log("in doc ready");
-  console.log(toF(userTemp));
-  console.log(toC(toF(userTemp)));
 
 
   // return temp in massive font in HTML
@@ -48,5 +45,7 @@ $(document).ready( function() {
     result = result.toFixed(2);
     $(document).find("h2").append(result+" &deg;F");
   }
+
+
 
 });
